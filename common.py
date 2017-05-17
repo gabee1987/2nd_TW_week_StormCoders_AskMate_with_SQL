289f444cc24527d6600b3f4f158707ba861791c2
BenceZambo <bence.zambo96@gmail.com>
2017-05-17 22:34:16 +0200

    db connection, and db execute functions replaced with data manager, and connection is closed after execution

--- common.py
+++ common.py
@@ -7,20 +7,15 @@ import psycopg2
 from local_config import *
 
 
-def db_connection():
+def database_manager(query, select_or_not):
     connect_str = "dbname={0} user={1} password={2} host={3}".format(DATABASE, USER, PASSWORD, HOST)
     conn = psycopg2.connect(connect_str)
     # set autocommit option, to do every query when we call it
     conn.autocommit = True
-    return conn
-
-conn = db_connection()
-
-
-def db_execute(query, conn):
     cursor = conn.cursor()
     cursor.execute(query)
-    records = cursor.fetchall()
-    return records
-
-
+    if select_type_query is True:
+        records = cursor.fetchall()
+        connection.close()
+        return records
+    connection.close()
