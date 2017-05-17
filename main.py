4b01dc6f6840f6b73a54aa37707106ff4bd900ff
BenceZambo <bence.zambo96@gmail.com>
2017-05-17 23:06:30 +0200

    new answer python file deleted and also rewrite to main function, and ready for some test

--- main.py
+++ main.py
@@ -98,20 +98,29 @@ def vote_down_question(q_id=None):
     pass
 
 
-@app.route('/question/<q_id>/new-answer', methods=['POST'])
+@app.route('/question/<q_id>/new-answer')
 def display_answer(q_id=None):
     '''
         Displays the answer form page.
     '''
-    pass
+    query = ("""SELECT title, message FROM question WHERE id={0};""".format(q_id))
+    select_type_query = True
+    view_questions = database_manager(query, select_type_query)
+    return render_template('answer_form.html', question_id=q_id, view_questions=view_questions)
 
 
-@app.route('/question/new-answer', methods=['POST'])
-def add_new_answer():
+@app.route('/question/new-answer/<q_id>', methods=['POST'])
+def add_new_answer(q_id=None):
     """
     Add the new answer to database.
     """
-    pass
+    dt = datetime.now()
+    answer_message = request.form["answer_message"]
+    query = ("""INSERT INTO answer(submisson_time, vote_number, question_id, message, image)
+                 VALUES({0}, {1}, {2}, {3}, {4}, {5}) WHERE id={6};""".format(dt, 0, q_id, answer_message, 0, q_id))
+    select_type_query = False
+    database_manager(query, select_type_query)
+    return redirect("/question/<q_id>")
 
 
 @app.errorhandler(404)
