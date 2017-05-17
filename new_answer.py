03b7f6ecea8505c677c469ff551e376049b201df
BenceZambo <bence.zambo96@gmail.com>
2017-05-17 23:01:11 +0200

    new answer python file updated with the new database manager system, and variable names changed in answer form html file

--- new_answer.py
+++ new_answer.py
@@ -4,9 +4,9 @@ def display_answer(q_id=None):
         Displays the answer form page.
     '''
     query = ("""SELECT title, message FROM question WHERE id={0};""".format(q_id))
-    conn = db_connection()
-    view_question_attributes = db_execute(query, conn)
-    return render_template('answer_form.html', question_id=q_id, view_question_attributes=view_question_attributes)
+    select_type_query = True
+    view_questions = database_manager(query, select_type_query)
+    return render_template('answer_form.html', question_id=q_id, view_questions=view_questions)
 
 
 @app.route('/question/new-answer/<q_id>', methods=['POST'])
@@ -18,6 +18,6 @@ def add_new_answer(q_id=None):
     answer_message = request.form["answer_message"]
     query = ("""INSERT INTO answer(submisson_time, vote_number, question_id, message, image)
                  VALUES({0}, {1}, {2}, {3}, {4}, {5}) WHERE id={6};""".format(dt, 0, q_id, answer_message, 0, q_id))
-    conn = db_connection()
-    db_execute(query, conn)
+    select_type_query = False
+    database_manager(query, select_type_query)
     return redirect("/question/<q_id>")
