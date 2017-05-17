911cbcfe8163e1772028820eb879a8905fee7bea
gabee1987 <gabee1987@gmail.com>
2017-05-17 10:20:17 +0200

    base64 module deleted

--- common.py
+++ common.py
@@ -13,42 +13,7 @@ import time
 from constants import (QUESTIONS_FILE, ANSWERS_FILE)
 
 
-def base64_decoder(data):
-    '''
-        Decodes the base64 question fields to readable string format.
-    '''
-    for row in data:
-        row[4] = base64.b64decode(row[4]).decode()
-        row[5] = base64.b64decode(row[5]).decode()
-    return data
 
-
-def base64_decoder_ans(data):
-    '''
-        Decodes the base64 answer fields to readable string format.
-    '''
-    for row in data:
-        row[4] = base64.b64decode(row[4]).decode()
-    return data
-
-
-def base64_encoder(data):
-    '''
-        Encodes the question string format to base64.
-    '''
-    for row in data:
-        row[4] = base64.b64encode(bytearray(row[4], encoding='utf-8')).decode()
-        row[5] = base64.b64encode(bytearray(row[5], encoding='utf-8')).decode()
-    return data
-
-
-def base64_encoder_ans(data):
-    '''
-        Encodes the answer string format to base64.
-    '''
-    for row in data:
-        row[4] = base64.b64encode(bytearray(row[4], encoding='utf-8')).decode()
-    return data
 
 
 
