5df7a424db5196c39c64705b0b5469ea91030196
BenceZambo <bence.zambo96@gmail.com>
2017-05-19 00:37:55 +0200

    unnecessary(test) files are deleted

--- queries.py
+++
@@ -1,9 +0,0 @@
-from common import *
-import psycopg2
-
-select_type_query = True
-query = """SELECT id, submission_time, vote_number, question_id, message, image FROM answer\
-                WHERE question_id = %s;"""
-values = '0'
-view_answers = database_manager(query, select_type_query, values)
-print(view_answers)
